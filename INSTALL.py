5a28aed8fd6afd7dd0321a38d1ea1de6291d59b5
Tylar <murray.tylar@gmail.com>
2014-06-10 12:34:43 -0400

    Update INSTALL.py

--- INSTALL.py
+++ INSTALL.py
@@ -1,6 +1,12 @@
 
 from subprocess import call
 
+call('sudo apt-get install python-gevent')
+
+call('sudo apt-get install python-pip') 
+
+call(['pip','install','gevent-websocket'])
+
 call('git submodule init')
 call('git submodule update')
 call('touch ./py/lib/bottle/__init__.py')
