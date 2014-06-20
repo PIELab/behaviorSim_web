1b3bbc18cbc6975d143ccd249358465edc40fb47
7yl4r <murray.tylar@gmail.com>
2014-06-20 13:30:28 -0400

    added graphical model selection screen. closes #19.

--- aws/connect.sh
+++ aws/connect.sh
@@ -1,6 +1,6 @@
 #ssh -i ~/Dropbox/AWS/tylarMurrayKey.pem ubuntu@{domain}
 #//that is..
 
-ssh -v -i ~/Documents/behaviorSim_web/aws/murrmedia_pair.pem ubuntu@ec2-54-209-234-200.compute-1.amazonaws.com #@testsubdomain.socialvinesolutions.com
+ssh -v -i ~/behaviorSim_web/aws/murrmedia_pair.pem ubuntu@ec2-54-209-234-200.compute-1.amazonaws.com #@testsubdomain.socialvinesolutions.com
 
 #//i think...
