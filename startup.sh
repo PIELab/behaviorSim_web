a6da33b044b4a7e20dfd6b2a361687b67b5f8e94
7yl4r <murray.tylar@gmail.com>
2014-10-30 10:07:52 -0400

    added agency template, some images, and less->css to startup script

--- startup.sh
+++ startup.sh
@@ -1,5 +1,9 @@
-echo 'compiling templates...'
+echo 'compiling dust templates...'
 duster templates templates/templates.js
+
+echo 'compiling less css...'
+lessc -x ./less/agency/agency.less > ./css/agency.min.css
+lessc -x ./less/AdminLTE.less > ./css/AdminLTE.css
 
 echo 'starting server for testing...'
 python -m SimpleHTTPServer
